530a0e6b195f4868ef85394a622a5c41a59fe742
konnano <73874687+konnano@users.noreply.github.com>
2022-09-25 13:15:40 +0900

    Update i2_trance.pl

--- i2_trance.pl
+++ i2_trance.pl
@@ -24,7 +24,7 @@ unless( $ARGV[0] ){
    }
   close $code;
   trans_1 $data,1; $data = '';
- }
+ }else{ print" Can't search file 1...\n"; }
 
  $file = -f 'Interfaces/PreferencePanel.xib' ? 'Interfaces/PreferencePanel.xib' : 0;
  if( $file ){
@@ -38,52 +38,65 @@ unless( $ARGV[0] ){
    }
   close $code;
   trans_1 $data,2; $data = '';
- }
+ }else{ print" Can't search file 2...\n"; }
+
+ $file = -f 'Interfaces/iTermPasteSpecialViewController.xib' ?
+            'Interfaces/iTermPasteSpecialViewController.xib' : 0;
+ if( $file ){
+  open my $code,'<',$file or die"3 $!";
+   while(<$code>){
+    $data .= /\s+title="([^"]+)"/   ? "$1\n" :
+             /\s+toolTip="([^"]+)"/ ? "$1\n" :
+             m|<string key="toolTip">(.+)</string>| ? "$1\n" : '';
+   }
+  close $code;
+  trans_1 $data,3; $data = '';
+ }else{ print" Can't search file 3...\n"; }
 
  $file = -f 'Interfaces/iTermEditKeyActionWindowController.xib' ?
             'Interfaces/iTermEditKeyActionWindowController.xib' : 0;
  if( $file ){
-  open my $code,'<',$file or die"3 $!\n";
+  open my $code,'<',$file or die"4 $!\n";
    while(<$code>){
     $data .= "$1\n" if /\s+title="([^"]+)"/;
    }
   close $code;
-  trans_1 $data,3; $data = '';
- }
+  trans_1 $data,4; $data = '';
+ }else{ print" Can't search file 4...\n"; }
 
  $file = -f 'sources/PointerPrefsController.m' ? 'sources/PointerPrefsController.m' : 0;
  if( $file ){
-  open my $code,'<',$file or die;
+  open my $code,'<',$file or die"5 $!\n";
    while(<$code>){
     $data .= "$1==$2\n" if /@"([^\s]+)\s+(.+)"/;
    }
   close $code;
   $data =~ s/-//g;
-  trans_1 $data,4; $data = '';
- }
+  trans_1 $data,5; $data = '';
+ }else{ print" Can't search file 5...\n"; }
 
  $file = -f 'sources/iTermKeyBindingMgr.m' ? 'sources/iTermKeyBindingMgr.m':
          -f 'sources/iTermKeyBindingAction.m' ? 'sources/iTermKeyBindingAction.m' : 0;
  if( $file ){
-  open my $code,'<',$file or die;
+  open my $code,'<',$file or die"6 $!\n";
    while(<$code>){
     $data .= "$1\n" if /actionString\s+=\s+@"([^"]+)"/;
    }
   close $code;
-  trans_1 $data,5; $data = '';
- }
+  trans_1 $data,6; $data = '';
+ }else{ print" Can't search file 6...\n"; }
 
  $file = -f 'sources/iTermAdvancedSettingsModel.m' ?
             'sources/iTermAdvancedSettingsModel.m' : 0;
  if( $file ){
-  open my $code,'<',$file or die"4 $!";
+  open my $code,'<',$file or die"7 $!";
    while(<$code>){
     $data .= "$1\n" if /SECTION_.+@"([^\\"]+)(?:\\n|")/;
     $data .= "$1\n" if /SECTION_[^\\]+\\n([^"]+)"/;
    }
   close $code;
-  trans_1 $data,6;
- }
+  trans_1 $data,7;
+ }else{ print" Can't search file 7...\n"; }
 
  unlink 'tran.txt';
 }
@@ -98,7 +111,7 @@ if( $ARGV[0] and $ARGV[0] == 1 ){
  }
    my( $e,$me,$file ) = 0;
 
- open my $trans,'<','trans.txt' or die"5 $!\n";
+ open my $trans,'<','trans.txt' or die"1_1 $!\n";
   my @bn = <$trans>;
  close $trans;
 
@@ -116,11 +129,11 @@ if( $ARGV[0] and $ARGV[0] == 1 ){
    }
   close $code;
   read_1 $me,$file; $me = '';
- }
+ }else{ print" Can't search file 1_1...\n"; }
 
  $file = -f 'Interfaces/PreferencePanel.xib' ? 'Interfaces/PreferencePanel.xib' : 0;
  if( $file ){
-  open my $code,'<',$file or die"7 $!";
+  open my $code,'<',$file or die"1_2 $!";
    while(my $data = <$code>){
     if( $data =~ /\s+title="[^"]+"/ ){
      for(;$e<@bn;){ chomp $bn[$e];
@@ -152,12 +165,39 @@ if( $ARGV[0] and $ARGV[0] == 1 ){
    }
   close $code;
   read_1 $me,$file; $me = '';
- }
+ }else{ print" Can't search file 1_2...\n"; }
+
+ $file = -f 'Interfaces/iTermPasteSpecialViewController.xib' ?
+            'Interfaces/iTermPasteSpecialViewController.xib' : 0;
+ if( $file ){
+  open my $code,'<',$file or die"1_3 $!";
+   while(my $data = <$code>){
+    if( $data =~ /\s+title="[^"]+"/ ){
+     for(;$e<@bn;){ chomp $bn[$e];
+      $data =~ s/title="[^"]+"/title="$bn[$e]"/;
+        $e++; last;
+     }
+    }elsif( $data =~ /<string key="toolTip">/ ){
+     for(;$e<@bn;){ chomp $bn[$e];
+      $data =~ s|.+|<string key="toolTip">$bn[$e]</string>|;
+        $e++; last;
+     }
+    }elsif( $data =~ /\s+toolTip="/ ){
+     for(;$e<@bn;){ chomp $bn[$e];
+      $data =~ s/toolTip="[^"]+"/toolTip="$bn[$e]"/;
+        $e++; last;
+     }
+    }
+    $me .= $data;
+   }
+  close $code;
+  read_1 $me,$file; $me = '';
+ }else{ print" Can't search file 1_3...\n"; }
 
  $file = -f 'Interfaces/iTermEditKeyActionWindowController.xib' ?
             'Interfaces/iTermEditKeyActionWindowController.xib' : 0;
  if( $file ){
-  open my $code,'<',$file or die"8 $!\n";
+  open my $code,'<',$file or die"1_4 $!\n";
    while(my $data = <$code>){
     if( $data =~ /title="[^"]+"/ ){
      for(;$e<@bn;){ chomp $bn[$e];
@@ -169,11 +209,11 @@ if( $ARGV[0] and $ARGV[0] == 1 ){
    }
   close $code;
   read_1 $me,$file; $me = '';
- }
+ }else{ print" Can't search file 1_4...\n"; }
 
  $file = -f 'sources/PointerPrefsController.m' ? 'sources/PointerPrefsController.m' : 0;
  if( $file ){
-  open my $code,'<',$file or die;
+  open my $code,'<',$file or die"1_5 $!\n";
    while(my $data = <$code>){
     if( $data =~ /@"[^\s]+\s+.+"/ ){
      for(;$e<@bn;){ #chomp $bn[$e];
@@ -186,12 +226,12 @@ if( $ARGV[0] and $ARGV[0] == 1 ){
    }
   close $code;
   read_1 $me,$file; $me = '';
- }
+ }else{ print" Can't search file 1_5...\n"; }
 
  $file = -f 'sources/iTermKeyBindingMgr.m' ? 'sources/iTermKeyBindingMgr.m':
          -f 'sources/iTermKeyBindingAction.m' ? 'sources/iTermKeyBindingAction.m' : 0;
  if( $file ){
-  open my $code,'<',$file or die;
+  open my $code,'<',$file or die"1_6 $!\n";
    while(my $data = <$code>){
     if( $data =~ /actionString\s+=\s+@"[^"]+"/ ){
      for(;$e<@bn;){ chomp $bn[$e];
@@ -203,12 +243,12 @@ if( $ARGV[0] and $ARGV[0] == 1 ){
    }
   close $code;
   read_1 $me,$file; $me = '';
- }
+ }else{ print" Can't search file 1_6...\n"; }
 
  $file = -f 'sources/iTermAdvancedSettingsModel.m' ?
             'sources/iTermAdvancedSettingsModel.m' : 0;
  if( $file ){
-  open my $code,'<',$file or die"9 $!";
+  open my $code,'<',$file or die"1_7 $!";
    while(my $data = <$code>){
     if( $data =~ /SECTION_.+@"[^\\"]+(?:\\n|")/ and $bn[$e] ){
      for(;$e<@bn;){ chomp $bn[$e];
@@ -232,5 +272,5 @@ if( $ARGV[0] and $ARGV[0] == 1 ){
    }
   close $code;
   read_1 $me,$file;
- }
+ }else{ print" Can't search file 1_7...\n"; }
 }
